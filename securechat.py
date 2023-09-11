13e54a6d8ab2c27209c542d5152529166aa3b27f
William Hunter <williamhunter@Williams-MacBook-Air.local>
2023-09-11 16:43:28 -0600

    single script prototype

--- securechat.py
+++ securechat.py
@@ -4,22 +4,38 @@ import sys
 
 isServer = False
 
+def error():
+    print("Error")
+    quit()
+
 context = zmq.Context()
 if len(sys.argv) == 1:
     isServer = True
     print("Binding server to port 5555")
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
+
 elif len(sys.argv) == 2:
     isServer = False
     print("Connecting to server at: " + sys.argv[1])
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://" + sys.argv[1] + ":5555")
 
+if isServer:
+    while True:
+        try:
+            message = socket.recv()
+        except:
+            error()
 
-while True:
-    message = socket.recv()
-    print("them: %s" % message)
+        print("them: %s" % message)
+        socket.send(b"World")
 
-    socket.send(b"World")
+else:
+    for request in range(10):
+        print("Sending request %s …" % request)
+        socket.send(b"Hello")
 
+        #  Get the reply.
+        message = socket.recv()
+        print("Received reply %s [ %s ]" % (request, message))
