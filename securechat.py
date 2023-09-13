b465c4fb448475d0905d988333935c689d9d5fbe
William Hunter <williamhunter@Williams-MacBook-Air.local>
2023-09-12 23:41:09 -0600

    two way chat but taking turns

--- securechat.py
+++ securechat.py
@@ -9,28 +9,35 @@ def error():
     quit()
 
 context = zmq.Context()
+pub = context.socket(zmq.PUB)
+sub = context.socket(zmq.SUB)
 if len(sys.argv) == 1:
     isServer = True
-    print("Binding server to port 5555")
-    socket = context.socket(zmq.PUB)
-    socket.bind("tcp://*:5555")
+    print("binding server to ports 5555 and 5556")
+    pub.bind("tcp://*:5555")
+    sub.bind("tcp://*:5556")
+    sub.setsockopt_string(zmq.SUBSCRIBE, "")
 #elif len(sys.argv) == 2:
 else:
     isServer = False
     print("Connecting to server at: " + sys.argv[1])
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://" + sys.argv[1] + ":5555")
-    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+    pub.connect("tcp://" + sys.argv[1] + ":5556")
+    sub.connect("tcp://" + sys.argv[1] + ":5555")
+    sub.setsockopt_string(zmq.SUBSCRIBE, "")
 
 if isServer:
     while True:
         message = input("> ")
-        socket.send(message.encode())
+        pub.send(message.encode())
+        message = sub.recv().decode()
+        print("them> ", message)
 
 else:
     while True:
-        message = socket.recv().decode()
+        message = sub.recv().decode()
         print("them> ", message)
+        message = input("> ")
+        pub.send(message.encode())
 
 
 
